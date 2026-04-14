f8b02c7560b1aa7d7694ef0b354ea5b417bfb069
MaxL34 <mlauvergeat@gmail.com>
2026-04-14 14:50:27 +0200

    Refactor: clean double code

--- app/models/user.py
+++ app/models/user.py
@@ -20,28 +20,6 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     is_admin = Column(Boolean, default=False)
 
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from sqlalchemy.sql import func
-from sqlalchemy.orm import relationship
-from app.database import Base
-
-class User(Base):
-    """SQLAlchemy model for a user in the application M-Motors.
-    
-    A user can be a cutomer : is_admin=Flase
-    Or an admin : is_admin=True
-    """
-    __tablename__="users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String(50), nullable=False)
-    last_name = Column(String(50), nullable=False)
-    email = Column(String(100), unique=True, index=True, nullable=False)
-    phone_number = Column(String(25), unique=True, index=True, nullable=True)
-    password_hash = Column(String(255), nullable=False)
-    is_active = Column(Boolean, default=True)
-    is_admin = Column(Boolean, default=False)
-
     # Automatically filled by the database when a new record is created or updated
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
