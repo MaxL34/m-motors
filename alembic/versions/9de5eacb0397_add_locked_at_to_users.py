8aa545fe0bee61d1445a975271a5bc83531c5c64
MaxL34 <mlauvergeat@gmail.com>
2026-05-04 11:23:15 +0200

    Migration: add purpose column to otp_codes to distinguish unlock vs reset OTPs

---
+++ alembic/versions/9de5eacb0397_add_locked_at_to_users.py
@@ -0,0 +1,21 @@
+"""add locked_at to users
+
+Revision ID: 9de5eacb0397
+Revises: c9d1e2f3a4b5
+Create Date: 2026-04-20 00:00:00.000000
+
+"""
+from typing import Sequence, Union
+
+revision: str = '9de5eacb0397'
+down_revision: Union[str, Sequence[str], None] = 'c9d1e2f3a4b5'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
